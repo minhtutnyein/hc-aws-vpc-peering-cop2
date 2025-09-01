6450c34941fb51bbfa35d4ea7cad0e4e5ab8bfd5
michale <minhtut.rocky@local>
2025-09-02 00:13:40 +0800

    updated security rules for both Frontend & Backend

--- backend_sgs.tf
+++ backend_sgs.tf
@@ -52,160 +52,138 @@ resource "aws_security_group" "backend_pri3_sg" {
 
 # # Security Rules for Backend_Pub1_SG
 # Ingress rules for backend_pub1_sg from Outsite (My IP)
-resource "aws_security_group_rule" "backend_pub1_ssh_from_outside_myIP" {
-  type              = "ingress"
+resource "aws_vpc_security_group_ingress_rule" "backend_pub1_ssh_from_outside_myIP" {
   from_port         = 22
   to_port           = 22
-  protocol          = "tcp"
+  ip_protocol       = "tcp"
   security_group_id = aws_security_group.backend_pub1_sg.id
-  cidr_blocks       = ["${chomp(data.http.myip.response_body)}/32"]
+  cidr_ipv4         = "${chomp(data.http.myip.response_body)}/32"
 }
 
-resource "aws_security_group_rule" "backend_pub1_icmp_from_outside_myIP" {
-  type              = "ingress"
+resource "aws_vpc_security_group_ingress_rule" "backend_pub1_icmp_from_outside_myIP" {
   from_port         = -1
   to_port           = -1
-  protocol          = "icmp"
+  ip_protocol       = "icmp"
   security_group_id = aws_security_group.backend_pub1_sg.id
-  cidr_blocks       = ["${chomp(data.http.myip.response_body)}/32"]
+  cidr_ipv4         = "${chomp(data.http.myip.response_body)}/32"
 }
 # Egress rules for backend_pub1_sg to Outsite (ANY)
-resource "aws_security_group_rule" "backend_pub1_ANY_to_outside_ANY" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
+resource "aws_vpc_security_group_egress_rule" "backend_pub1_ANY_to_outside_ANY" {
+  ip_protocol       = "-1"
   security_group_id = aws_security_group.backend_pub1_sg.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_ipv4         = "0.0.0.0/0"
 }
 # # Security Rules for backend_pri1_SG
-resource "aws_security_group_rule" "backend_pub1_ssh_to_backend_pri1" {
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.backend_pri1_sg.id
-  source_security_group_id = aws_security_group.backend_pub1_sg.id
+resource "aws_vpc_security_group_ingress_rule" "backend_pub1_ssh_to_backend_pri1" {
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  security_group_id = aws_security_group.backend_pri1_sg.id
+  cidr_ipv4         = aws_subnet.backend_public["backend_publicSubnet_1"].cidr_block
 }
 
-resource "aws_security_group_rule" "backend_pub1_icmp_to_backend_pri1" {
-  type                     = "ingress"
-  from_port                = -1
-  to_port                  = -1
-  protocol                 = "icmp"
-  security_group_id        = aws_security_group.backend_pri1_sg.id
-  source_security_group_id = aws_security_group.backend_pub1_sg.id
-}
-
-resource "aws_security_group_rule" "backend_pri1_ANY_to_outside_ANY" {
-  type              = "egress"
+resource "aws_vpc_security_group_ingress_rule" "backend_pub1_icmp_to_backend_pri1" {
   from_port         = 0
   to_port           = 0
-  protocol          = "-1"
+  ip_protocol       = "icmp"
   security_group_id = aws_security_group.backend_pri1_sg.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_ipv4         = aws_subnet.backend_public["backend_publicSubnet_1"].cidr_block
+}
+
+resource "aws_vpc_security_group_egress_rule" "backend_pri1_ANY_to_outside_ANY" {
+  ip_protocol       = "-1"
+  security_group_id = aws_security_group.backend_pri1_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
 }
 # # Create Security Rules for Backend_Pri2_SG 
 # # # Create Ingress Security Rules 
-resource "aws_security_group_rule" "backend_pri1_to_ping_private_instance_02" {
-  type                     = "ingress"
-  from_port                = -1
-  to_port                  = -1
-  protocol                 = "icmp"
-  security_group_id        = aws_security_group.backend_pri2_sg.id
-  source_security_group_id = aws_security_group.backend_pri1_sg.id
+resource "aws_vpc_security_group_ingress_rule" "backend_pri1_to_ping_private_instance_02" {
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = "icmp"
+  security_group_id = aws_security_group.backend_pri2_sg.id
+  cidr_ipv4         = aws_subnet.backend_private["backend_privateSubnet_1"].cidr_block
 }
-resource "aws_security_group_rule" "backend_pri1_to_ssh_private_instance_02" {
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.backend_pri2_sg.id
-  source_security_group_id = aws_security_group.backend_pri1_sg.id
+resource "aws_vpc_security_group_ingress_rule" "backend_pri1_to_ssh_private_instance_02" {
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  security_group_id = aws_security_group.backend_pri2_sg.id
+  cidr_ipv4         = aws_subnet.backend_private["backend_privateSubnet_1"].cidr_block
 }
 
-resource "aws_security_group_rule" "frontend_pri2_ping_to_backend_private_instance_02_ingress" {
-  type                     = "ingress"
-  from_port                = -1
-  to_port                  = -1
-  protocol                 = "icmp"
-  security_group_id        = aws_security_group.backend_pri2_sg.id
-  source_security_group_id = aws_security_group.frontend_pri2_sg.id
+resource "aws_vpc_security_group_ingress_rule" "frontend_pri2_ping_to_backend_private_instance_02_ingress" {
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = "icmp"
+  security_group_id = aws_security_group.backend_pri2_sg.id
+  cidr_ipv4         = aws_subnet.frontend_private["frontend_privateSubnet_2"].cidr_block
 }
-resource "aws_security_group_rule" "frontend_pri2_ssh_to_backend_private_instance_02_ingress" {
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.backend_pri2_sg.id
-  source_security_group_id = aws_security_group.frontend_pri2_sg.id
+resource "aws_vpc_security_group_ingress_rule" "frontend_pri2_ssh_to_backend_private_instance_02_ingress" {
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  security_group_id = aws_security_group.backend_pri2_sg.id
+  cidr_ipv4         = aws_subnet.frontend_private["frontend_privateSubnet_2"].cidr_block
 }
 # # # Create Egress Security Rules 
-resource "aws_security_group_rule" "backend_pri2_ping_to_frontend_private_instance_02_egress" {
-  type                     = "egress"
-  from_port                = -1
-  to_port                  = -1
-  protocol                 = "icmp"
-  security_group_id        = aws_security_group.backend_pri2_sg.id
-  source_security_group_id = aws_security_group.frontend_pri2_sg.id
+resource "aws_vpc_security_group_egress_rule" "backend_pri2_ping_to_frontend_private_instance_02_egress" {
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = "icmp"
+  security_group_id = aws_security_group.backend_pri2_sg.id
+  cidr_ipv4         = aws_subnet.frontend_private["frontend_privateSubnet_2"].cidr_block
 }
-resource "aws_security_group_rule" "backend_pri2_ssh_to_frontend_private_instance_02_egress" {
-  type                     = "egress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.backend_pri2_sg.id
-  source_security_group_id = aws_security_group.frontend_pri2_sg.id
+resource "aws_vpc_security_group_egress_rule" "backend_pri2_ssh_to_frontend_private_instance_02_egress" {
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  security_group_id = aws_security_group.backend_pri2_sg.id
+  cidr_ipv4         = aws_subnet.frontend_private["frontend_privateSubnet_2"].cidr_block
 }
 # # Create Security Rules for Backend_Pri3_SG
 # # # Create Ingress Security Rules
-resource "aws_security_group_rule" "backend_pri1_to_ping_private_instance_03" {
-  type                     = "ingress"
-  from_port                = -1
-  to_port                  = -1
-  protocol                 = "icmp"
-  security_group_id        = aws_security_group.backend_pri3_sg.id
-  source_security_group_id = aws_security_group.backend_pri1_sg.id
+resource "aws_vpc_security_group_ingress_rule" "backend_pri1_to_ping_private_instance_03" {
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = "icmp"
+  security_group_id = aws_security_group.backend_pri3_sg.id
+  cidr_ipv4         = aws_subnet.backend_private["backend_privateSubnet_1"].cidr_block
 }
-resource "aws_security_group_rule" "backend_pri1_to_ssh_private_instance_03" {
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.backend_pri3_sg.id
-  source_security_group_id = aws_security_group.backend_pri1_sg.id
+resource "aws_vpc_security_group_ingress_rule" "backend_pri1_to_ssh_private_instance_03" {
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  security_group_id = aws_security_group.backend_pri3_sg.id
+  cidr_ipv4         = aws_subnet.backend_private["backend_privateSubnet_1"].cidr_block
 }
 
-resource "aws_security_group_rule" "frontend_pri3_ping_to_backend_private_instance_03_ingress" {
-  type                     = "ingress"
-  from_port                = -1
-  to_port                  = -1
-  protocol                 = "icmp"
-  security_group_id        = aws_security_group.backend_pri3_sg.id
-  source_security_group_id = aws_security_group.frontend_pri3_sg.id
+resource "aws_vpc_security_group_ingress_rule" "frontend_pri3_ping_to_backend_private_instance_03_ingress" {
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = "icmp"
+  security_group_id = aws_security_group.backend_pri3_sg.id
+  cidr_ipv4         = aws_subnet.frontend_private["frontend_privateSubnet_3"].cidr_block
 }
-resource "aws_security_group_rule" "frontend_pri3_ssh_to_backend_private_instance_03_ingress" {
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.backend_pri3_sg.id
-  source_security_group_id = aws_security_group.frontend_pri3_sg.id
+resource "aws_vpc_security_group_ingress_rule" "frontend_pri3_ssh_to_backend_private_instance_03_ingress" {
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  security_group_id = aws_security_group.backend_pri3_sg.id
+  cidr_ipv4         = aws_subnet.frontend_private["frontend_privateSubnet_3"].cidr_block
 }
 # # # Create Egress Security Rules
-resource "aws_security_group_rule" "backend_pri3_ping_to_frontend_private_instance_03_egress" {
-  type                     = "egress"
-  from_port                = -1
-  to_port                  = -1
-  protocol                 = "icmp"
-  security_group_id        = aws_security_group.backend_pri3_sg.id
-  source_security_group_id = aws_security_group.frontend_pri3_sg.id
+resource "aws_vpc_security_group_egress_rule" "backend_pri3_ping_to_frontend_private_instance_03_egress" {
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = "icmp"
+  security_group_id = aws_security_group.backend_pri3_sg.id
+  cidr_ipv4         = aws_subnet.frontend_private["frontend_privateSubnet_3"].cidr_block
 }
-resource "aws_security_group_rule" "backend_pri3_ssh_to_frontend_private_instance_03_egress" {
-  type                     = "egress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.backend_pri3_sg.id
-  source_security_group_id = aws_security_group.frontend_pri3_sg.id
+resource "aws_vpc_security_group_egress_rule" "backend_pri3_ssh_to_frontend_private_instance_03_egress" {
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  security_group_id = aws_security_group.backend_pri3_sg.id
+  cidr_ipv4         = aws_subnet.frontend_private["frontend_privateSubnet_3"].cidr_block
 }
